47b4d5b6def66e5368adc9564126956005c02581
dejef <dejef@pdx.edu>
2020-05-18 22:21:45 -0700

    React-Bootstrap with example cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card'
+
+
 import './App.css';
 
 class App extends React.Component {
@@ -9,9 +15,41 @@ class App extends React.Component {
   }
 
   render() {
+    let x = [
+      'Primary',
+      'Secondary',
+      'Success',
+      'Danger',
+      'Warning',
+      'Info',
+      'Light',
+      'Dark',
+    ].map((variant, idx) => (
+      <>
+        <Card
+          bg={variant.toLowerCase()}
+          key={idx}
+          text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+          style={{ width: '18rem' }}
+        >
+          <Card.Header>Header</Card.Header>
+          <Card.Body>
+            <Card.Title>{variant} Card Title </Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <br />
+      </>
+    ));
+
     return (
       <div>
-        working on trails
+        <Container className="p-3">
+          {x}
+        </Container> 
       </div>
     )
   }
